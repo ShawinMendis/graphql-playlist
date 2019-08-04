0bee45a85191e231d87f8c702906dbf2f0f0866a
Shawin-Mendis-at-743015932749 <shawinshawz@gmail.com>
2019-08-05 01:13:24 +1000

    inital commit

---
+++ server/app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const schema = require("./schema/schema");
+const mongoose = require("mongoose");
+
+const app = express();
+
+mongoose.connect(
+  `mongodb+srv://shawin-mendis:Shawwa19946@cluster0-08ss5.mongodb.net/test?retryWrites=true&w=majority`
+);
+mongoose.connection.once("open", () => {
+  console.log("connected to dtabase");
+});
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: true
+  })
+);
+
+app.listen(5000, () => {
+  console.log("now listening on port 4000");
+});
